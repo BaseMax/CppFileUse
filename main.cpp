c535200109191f73cd9b7a084d06b6823eea810f
Max Base <MaxBaseCode@Gmail.Com>
2019-06-26 13:51:14 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : CppFileUse/main.cpp
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-06-26
+* @Released under : https://github.com/BaseMax/CppFileUse/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/CppFileUse
+*
+**/
